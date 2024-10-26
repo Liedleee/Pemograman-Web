0cc7efee5ea1ca419111d949ad1630b8378a7085
Liedlel <nursalilliedle@gmail.com>
2024-10-26 16:16:01 +0700

    m3

--- Modul3/Codelab/segitiga.php
+++ Modul3/Codelab/segitiga.php
@@ -3,12 +3,12 @@ $height = 5; // Tinggi segitiga
 for($i = 1; $i <= $height; $i++) {
     // Cetak spasi
     for($j = $height; $j > $i; $j--) {
-        echo " ";
+        echo "&nbsp";
     }
     // Cetak bintang
     for($k = 1; $k <= (2 * $i - 1); $k++) {
         echo "*";
     }
-    echo "\n"; // Pindah ke baris baru
+    echo "<br>"; // Pindah ke baris baru
 }
 ?>
---
+++ Modul3/Codelab/segitiga2.php
@@ -0,0 +1,18 @@
+<?php
+function segitigaTerbalik($tinggi) {
+    for ($i = $tinggi; $i >= 1; $i--) {
+        // Mencetak spasi (gunakan &nbsp; untuk spasi dalam HTML)
+        for ($j = $tinggi; $j > $i; $j--) {
+            echo "&nbsp";
+        }
+        // Mencetak bintang
+        for ($k = 1; $k <= (2 * $i - 1); $k++) {
+            echo "*";
+        }
+        // Pindah ke baris berikutnya dengan <br>
+        echo "<br>";
+    }
+}
+
+// Panggil fungsi dengan tinggi segitiga 5
+segitigaTerbalik(5);
